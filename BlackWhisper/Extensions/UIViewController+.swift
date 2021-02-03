fe72e61820b553eadc1e86366a7f8d445df5b8ff
dkuyoshi <daiki1213@stu.kanazawa-u.ac.jp>
2021-02-03 17:51:00 +0900

    add extensions

---
+++ BlackWhisper/Extensions/UIViewController+.swift
@@ -0,0 +1,20 @@
+//
+//  UIViewController+.swift
+//  BlackWhisper
+//
+//  Created by Daiki Kuyoshi on 2021/02/03.
+//
+
+import UIKit
+
+extension UIViewController {
+    func hideKeyboardWhenTappedAround() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIViewController.hideKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+
+    @objc func hideKeyboard() {
+        view.endEditing(true)
+    }
+}
