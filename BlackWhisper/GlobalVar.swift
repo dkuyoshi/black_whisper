cdb4db3d58966c2b1ef7b16e1c0807ddc7ef55b2
dkuyoshi <daiki1213@stu.kanazawa-u.ac.jp>
2021-02-04 22:47:09 +0900

    add 広域変数

---
+++ BlackWhisper/GlobalVar.swift
@@ -0,0 +1,19 @@
+//
+//  GlobalVar.swift
+//  BlackWhisper
+//
+//  Created by Daiki Kuyoshi on 2021/02/04.
+//
+
+open class GlobalVar {
+    private init() {}
+    static let shared = GlobalVar()
+
+    var count: Int = 0
+    var point: Int = 0
+    var clear: Int = 0
+    
+    var userNames: [String] = Array(repeating: "", count: 4)
+    var positionArray: [Int] = []
+    var csvArray: [String] = []
+}
